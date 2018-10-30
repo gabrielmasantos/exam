1a2e0a376e01c723ad6298fa1b7308c06e2e272c
Gabriel Santos <gabrielsantos@Gabriels-MacBook.local>
2018-10-30 17:04:32 +0100

    demo

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -110,7 +110,7 @@ public class UserController {
     }
 
     // Insert the user in the DB
-    // TODO: Hash the user password before saving it.
+    // TODO: Hash the user password before saving it. //DEOMOEMDOME
     int userID = dbCon.insert(
         "INSERT INTO user(first_name, last_name, password, email, created_at) VALUES('"
             + user.getFirstname()
